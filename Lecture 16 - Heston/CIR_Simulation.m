2cb760039973c0658f1892e1040c9042304f3d69
Noé <noe.debrois@student-cs.fr>
2024-11-17 15:11:34 +0100

    Heston Model

--- Lecture 16 - Heston/CIR_Simulation.m
+++ Lecture 16 - Heston/CIR_Simulation.m
@@ -1,24 +1,45 @@
-clear
-close all
-% parameters
-y0=0.02;
-beta=0.3; lambda=1; eta=0.05;
-if beta^2-2*lambda*eta<=0
+%% LECTURE 16 - Heston Model - Noé Debrois - 16/11/2024
+% This code implements the sampling of CIR using Euler Scheme. We can see,
+% if we zoom, non-zero probability to go below 0, whereas the theory says
+% it's impossible : NUMERICAL ERROR (see the warning in the command line
+% about "Imaginary Part". So we prevent it by a max(y, 0)...
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+clear; close all;
+%
+% t_i = i * dt, let's discretize the SDE from (y_t)_t :
+% y_{i+1} = y_{i} + lambda * (eta - y_{i}) * dt + beta * sqrt(y_{i}) *
+% sqrt(t) * Z, Z ~ N(0, 1) [EULER SCHEME].
+%
+%% Parameters :
+y0 = 0.02;
+beta = 0.3;
+lambda = 1;
+eta = 0.05;
+
+if beta^2 - 2 * lambda * eta <= 0
     disp('Feller condition satisfied')
+    % P(y_t = 0) = 0 & P(y_t < 0) = 0
 else
     disp('Feller condition NOT satisfied')
+    % P(y_t = 0) > 0 & P(y_t < 0) = 0
 end
-T=1; M=100; Nsim=100; 
-dt=T/M;
-y=zeros(Nsim,M+1); y(:,1)=y0;
+
+T = 1;
+M = 100;
+dt = T / M;
+Nsim = 50; 
+%
+%% Sampling CIR using Euler Scheme :
+y = zeros(Nsim, M + 1); 
+y(:, 1) = y0; % Initial condition
+
 for i=1:M
-    y(:,i+1)=y(:,i)+lambda*(eta-y(:,i))*dt+...
-             beta*sqrt(y(:,i))*sqrt(dt).*randn(Nsim,1);
-    y(:,i+1)=max(y(:,i+1),0);     
+    y(:, i + 1) = y(:, i) + lambda * (eta - y(:, i)) * dt +...
+        beta * sqrt(y(:, i)) * sqrt(dt) .* randn(Nsim, 1);
+    y(:, i + 1) = max(y(:, i + 1), 0); % Prevent from falling below 0 !
 end
-t=linspace(0,T,M+1); plot(t,y);
-
-
-
-
-
+%
+%% Plot :
+t = linspace(0, T, M + 1);
+plot(t, y);
+title("Realisations of (y_t)_t (CIR) using Euler Scheme")
--- Lecture 16 - Heston/Heston.m
+++ Lecture 16 - Heston/Heston.m
@@ -1,48 +1,61 @@
-% Simulate the Heston model via the Euler scheme
+%% LECTURE 16 - Heston Model - Noé Debrois - 16/11/2024
+% This code implements the simulation the Heston model via the Euler scheme
+% (not the article from Andersen). So it is like in CIR_Simulation.m, but
+% with also the simulation of X (using V). Of course, V can be negative,
+% even when Feller condition is satisfied, which is the main problem of
+% this method. We need more advanced schemes to solve this (cf Andersen).
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 clear; close all;
-% Input
-%>variance
-V0=0.1; % variance at time 0
-theta=0.3; % mean 
-k=0.1; % speed of mean reversion
-epsilon=0.2; %vol-of-var
-%>price
-r=0.02;
-S0=100; 
-%>correlation between price and variance
-rho=-0.6;
-% Calibration
-%> r, S0 observed on the market
-%> [V0, theta, k, epsilon, rho] have to be calibrated (from European Call
-%exploiting Carr-Madan --> characteristic function of Heston known)
+%
+%% Inputs :
+% Variance : 
+V0 = 0.1; % Variance at time 0.
+theta = 0.3; % Mean.
+k = 0.1; % Speed of mean reversion.
+epsilon = 0.2; % Vol-of-var.
 
-Feller_condition=( epsilon^2-2*k*theta )
-if Feller_condition<=0
+% Price :
+r = 0.02;
+S0 = 100; 
+
+% Correlation between price and variance :
+rho = -0.6;
+
+% Calibration :
+% -> r, S0 : observed on the market ;
+% -> [V0, theta, k, epsilon, rho] have to be calibrated (from European Call
+% exploiting Carr-Madan --> characteristic function of Heston known).
+
+Feller_condition = (epsilon^2 - 2 * k * theta)
+if Feller_condition <= 0
     disp('Feller Condition is satisfied')
 else
     disp('Feller Condition is NOT satisfied')
 end
-Nsim=1e4; T=1; Nsteps=25; %discretization
-% MC simulation
-dt=T/Nsteps; t=linspace(0,T,Nsteps+1); % time grid
-x=zeros(Nsim,Nsteps+1); x(:,1)=log(S0); %log price process
-V=zeros(Nsim,Nsteps+1); V(:,1)=V0; %variance process
-% mean and variance-covariance matrix of the normal distribution to be sampled
-mu=[0;0]; VC=[1 rho;rho 1]; 
+
+% Discretization :
+Nsim = 1e4; T = 1; Nsteps = 25;
+
+% MC simulation :
+dt = T / Nsteps; t = linspace(0, T, Nsteps + 1); % Time grid.
+x = zeros(Nsim, Nsteps + 1); x(:, 1) = log(S0); % Log price process.
+V = zeros(Nsim, Nsteps + 1); V(:, 1) = V0; % Variance process.
+%
+%% Mean & variance-covariance matrix of normal distribution to be sampled :
+mu = [0; 0]; VC = [1 rho; rho 1]; % Covariance matrix between x and V.
 for i=1:Nsteps
-    Z=mvnrnd(mu,VC,Nsim); 
-    x(:,i+1)=x(:,i)+(r-V(:,i)/2)*dt+sqrt(V(:,i)*dt).*Z(:,1);
-    V(:,i+1)=V(:,i)+k*(theta-V(:,i))*dt+...
-        epsilon*sqrt(V(:,i)*dt).*Z(:,2);
+    Z = mvnrnd(mu, VC, Nsim); % Multivariate normal random numbers.
+    x(:, i + 1) = x(:, i) + (r - V(:, i) / 2) * dt +...
+        sqrt(V(:, i) * dt) .* Z(:, 1);
+    %
+    V(:, i + 1) = V(:, i) + k * (theta - V(:, i)) * dt +...
+        epsilon * sqrt(V(:, i) * dt) .* Z(:, 2);
 end
-S=exp(x);
+
+% Computation of the price from the log-price :
+S = exp(x);
+%
+%% Plot :
 figure; hold on
-subplot(1,2,1); plot(t,S); title('Price');
-subplot(1,2,2); plot(t,V); title('Variance');
-
-
-    
-    
-
-
-
+subplot(1, 2, 1); plot(t, S); title('Price');
+subplot(1, 2, 2); plot(t, V); title('Variance');
