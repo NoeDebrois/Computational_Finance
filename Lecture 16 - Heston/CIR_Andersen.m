913a77d95017ded95db03a66b658ce456ea15b6b
Noé <noe.debrois@student-cs.fr>
2024-11-22 12:18:45 +0100

    Lecture 16 - Code from Andersen's article for Heston Model

--- Lecture 16 - Heston/CIR_Andersen.m
+++ Lecture 16 - Heston/CIR_Andersen.m
@@ -1,54 +1,63 @@
-clear 
-V0=0.01; 
-k=2; % speed of mean reversion
-theta=0.002; % long-term mean reversion
-epsilon=0.2; % vol-of-vol
+%% LECTURE 16 - Heston Model - Noé Debrois - 22/11/2024
+% This code implements the QE Scheme for Heston model, from the article of
+% Andersen : "Efficient Simulation of the Heston Stochastic Volatility 
+% Model". -> A fully commented function version is available : Heston_QE.m.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+clear; close all;
+%--- PARAMETERS -----------------------------------------------------------
+V0 = 0.01; % Variance at time 0.
+k = 2; % Speed of mean reversion.
+theta = 0.002; % Long-term mean.
+epsilon = 0.2; % Vol-of-var.
 
-Nsim=10;
-M=100; T=1;
-dt=T/M;
+Nsim = 10; % Nb of sims.
+M = 100; T = 1; dt = T / M; % Time grid.
 
-V=zeros(Nsim,M+1); V(:,1)=V0;
-% choose Psi_cutoff = 1.5 as suggested in article
+V = zeros(Nsim, M + 1); V(:, 1) = V0; % Variance.
+
+% Choose Psi_cutoff = 1.5 as suggested in article :
 Psi_cutoff = 1.5;
+
 %--- DISCRETIZE V --------------------------------------------------------
 for i=1:M
     % STEP 1 & 2
     % Calculate m,s,Psi
-    m=theta+(V(:,i)-theta)*exp(-k*dt);
-    m2=m.^2;
-    s2=V(:,i)*epsilon^2*exp(-k*dt)*...
-        (1-exp(-k*dt))/k+...
-        theta*epsilon^2*...
-        (1-exp(-k*dt))^2/(2*k);
-    s=sqrt(s2);
+    m = theta + (V(:, i) - theta) * exp(-k * dt);
+    m2 = m.^2;
+
+    s2 = V(:, i) * epsilon^2 * exp(-k * dt) * ...
+        (1 - exp(-k * dt)) / k + ...
+        theta * epsilon^2 * ...
+        (1 - exp(-k * dt))^2 / (2 * k);
+    s = sqrt(s2);
     
     Psi = (s2)./(m2);
     
     % STEP 3,4,5
     % Depending on Psi, use exp or quad scheme to calculate next V
-    index=find(Psi>Psi_cutoff);
+    index = find(Psi > Psi_cutoff);% Detection for E scheme.
+
     % Exponential approximation, used for Psi>Psi_cutoff)
     % PDF of V(t+dt) is p*delta(0) + (1-p) * (1-exp(-beta x )
     % thus a prob. mass in 0 & an exp tail after that
-    p_exp = (Psi(index)-1)./(Psi(index)+1);	% Prob. mass in 0, Eq 29
-    beta_exp = (1-p_exp)./m(index);		% exponent of exp. density tail, Eq 30
-    % gets x from inverse CDF applied to uniform U
+    p_exp = (Psi(index) - 1) ./ (Psi(index) + 1 ); % Prob. mass in 0, Eq 29
+    beta_exp = (1 - p_exp) ./ m(index);	% Exponent of exp. density tail, Eq 30
+    % Gets x from inverse CDF applied to uniform U
     U = rand(size(index));
-    V(index,i+1) = ...
-(log((1-p_exp)./(1-U))./beta_exp)...
-.*(U>p_exp);
+    V(index, i + 1) = (log((1 - p_exp) ./ (1 - U)) ./ beta_exp) .* (U > p_exp);
 
-    index=find(Psi<=Psi_cutoff);
+    index = find(Psi <= Psi_cutoff); % Detection for Q scheme.
     % Quadratic approx, used for 0<Psi<Psi_cutoff
     % V(t+dt) = a(b+Zv)^2, Zv~N(0,1)
-    invPsi = 1./Psi(index);
-    b2_quad = 2*invPsi-1+sqrt(2*invPsi).*sqrt(2*invPsi-1); % Eq 27
-    a_quad = m(index)./(1+b2_quad);	% Eq 28
-    V(index,i+1) = ...
- a_quad.*(sqrt(b2_quad)+randn(size(index))).^2; % Eq.23
-    
+    invPsi = 1 ./ Psi(index);
+    % Eq 27 :
+    b2_quad = 2 * invPsi - 1 + sqrt(2 * invPsi) .* sqrt(2 * invPsi - 1);
+    % Eq 28 :
+    a_quad = m(index) ./ (1 + b2_quad);	
+    % Eq.23 :
+    V(index,i+1) = a_quad .* (sqrt(b2_quad) + randn(size(index))) .^2; 
 end
-t=linspace(0,T,M+1);
-plot(t,V)
-    
+
+%--- PLOT -----------------------------------------------------------------
+t = linspace(0, T, M + 1);
+plot(t, V)
--- Lecture 16 - Heston/Heston_QE.m
+++ Lecture 16 - Heston/Heston_QE.m
@@ -1,70 +1,89 @@
-function X=Heston_QE(X0,r,x,T,Nsim,N)
-% Implementation of QE Scheme for Heston model
-% "Efficient Simulation of the Heston Stochastic Volatility Model",Andersen
+%% LECTURE 16 - Heston Model - Noé Debrois - 22/11/2024
+% This code implements the QE Scheme for Heston model, from the article of
+% Andersen : "Efficient Simulation of the Heston Stochastic Volatility 
+% Model".
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% PARAMETERS
-epsilon=x(1); % vol-of-vol
-k=x(2); % mean reversion speed
-rho=x(3); % correlation
-theta=x(4); % mean
-V0=x(5);
-
-dt=T/N; 
-V = zeros(N+1,Nsim); X = zeros(N+1,Nsim);
+function X = Heston_QE(X0, r, x, T, Nsim, N)
+%--- PARAMETERS -----------------------------------------------------------
+epsilon = x(1); % Vol-of-var.
+k = x(2); % Speed of mean reversion speed.
+rho = x(3); % Correlation between price and variance.
+theta = x(4); % Mean.
+dt = T / N; % Time grid.
+V0 = x(5); % Variance at time 0.
+V = zeros(N + 1, Nsim); % Variance. 
+X = zeros(N + 1, Nsim); % Price.
 
 % Random changes for volatility are sampled from one of two distributions,
-% depending on the ratio Psi = s^2/m^2, where m & s are mean and variance
+% depending on the ratio Psi = s^2 / m^2, where m & s are mean and variance
 % of next volatility value, conditioned to current one.
-% Scheme 1 (exponential approx.) selected when Psi>Psi_cutoff
-% Scheme 2 (quadratic approx.) selected when 0<Psi<Psi_cutoff
+% - Scheme 1 (exponential approx.) selected when Psi > Psi_cutoff "E" ;
+% - Scheme 2 (quadratic approx.) selected when 0 < Psi < Psi_cutoff "Q".
 
-% choose Psi_cutoff = 1.5 as suggested in article
+% Choose Psi_cutoff = 1.5 as suggested in article :
 Psi_cutoff = 1.5;
 
 %--- DISCRETIZE V ---------------------------------------------------------
-V(1,:)=V0;
+V(1,:) = V0;
 for i=1:N
-    % STEP 1 & 2
-    % Calculate m,s,Psi
-    m=theta+(V(i,:)-theta)*exp(-k*dt);
-    m2=m.^2;
-    s2=V(i,:)*epsilon^2*exp(-k*dt)*(1-exp(-k*dt))/k+...
-        theta*epsilon^2*(1-exp(-k*dt))^2/(2*k);
-    s=sqrt(s2);
-    
+    % STEP 1 & 2 :
+    % Calculate m, s, cf slide 18 :
+    m = theta + (V(i, :) - theta) * exp(-k * dt);
+    m2 = m.^2;
+    % cf slide 18 :
+    s2 = V(i, :) * epsilon^2 * exp(-k * dt) * (1 - exp(-k * dt)) / k +...
+        theta * epsilon^2 * (1 - exp(-k * dt))^2 / (2 * k);
+    s = sqrt(s2);
+
+    % Compute Psi :
     Psi = (s2)./(m2);
     
-    % STEP 3,4,5
-    % Depending on Psi, use exp or quad scheme to calculate next V
-    index=find(Psi>Psi_cutoff);
-    % Exponential approximation, used for Psi>Psi_cutoff)
-    % PDF of V(t+dt) is p*delta(0) + (1-p) * (1-exp(-beta x )
-    % thus a prob. mass in 0 & an exp tail after that
-    p_exp = (Psi(index)-1)./(Psi(index)+1);	% Prob. mass in 0, Eq 29
-    beta_exp = (1-p_exp)./m(index);		% exponent of exp. density tail, Eq 30
-    % gets x from inverse CDF applied to uniform U
-    U = rand(size(index));
-    V(i+1,index) = (log((1-p_exp)./(1-U))./beta_exp).*(U>p_exp);
+    % STEP 3, 4, 5 :
+    % Depending on Psi, use exp (E) or quad (Q) scheme to calculate next V:
+    index = find(Psi > Psi_cutoff); % Detection for E scheme.
 
-    index=find(Psi<=Psi_cutoff);
-    % Quadratic approx, used for 0<Psi<Psi_cutoff
-    % V(t+dt) = a(b+Zv)^2, Zv~N(0,1)
-    invPsi = 1./Psi(index);
-    b2_quad = 2*invPsi-1+sqrt(2*invPsi).*sqrt(2*invPsi-1); % Eq 27
-    a_quad = m(index)./(1+b2_quad);	% Eq 28
-    V(i+1,index) = a_quad.*(sqrt(b2_quad)+randn(size(index))).^2; % Eq.23
-    
+    % Exponential approximation, used for Psi>Psi_cutoff) : CF SLIDE 23.
+    %
+    % PDF of V(t+dt) is p * delta(0) + (1 - p) * (1 - exp(-beta * x)
+    % thus a prob. mass in 0 & an exp tail after that :
+    p_exp = (Psi(index)-1)./(Psi(index)+1);	% Prob. mass in 0, Eq 29 ;
+    beta_exp = (1-p_exp)./m(index); % Exponent of exp. density tail, Eq 30;
+
+    % Gets x from inverse CDF applied to uniform U (SLIDE 21) :
+    U = rand(size(index));
+    V(i + 1, index) = (log((1 - p_exp) ./ (1 - U)) ./ beta_exp) .* (U > p_exp);
+
+    index = find(Psi <= Psi_cutoff); % Detection for Q scheme.
+
+    % Quadratic approximation, used for 0 < Psi < Psi_cutoff : CF SLIDE 23.
+    %
+    % V(t + dt) = a * (b + Zv)^2, Zv ~ N(0, 1). SLIDE 24.
+    % From Slide 23 :
+    invPsi = 1 ./ Psi(index);
+    % Eq 27 : 
+    b2_quad = 2 * invPsi - 1 + sqrt(2 * invPsi) .* sqrt(2 * invPsi - 1);
+    % Eq 28 :
+    a_quad = m(index) ./ (1 + b2_quad);	
+    % Eq.23 (slide 24) :
+    V(i + 1, index) = a_quad .* (sqrt(b2_quad) + randn(size(index))) .^2 ;
 end
 
-%-- DISCRETIZE X ----------------------------------------------------------
-X(1,:)=X0;
-gamma1=0.5; gamma2=0.5; %central discretization scheme
-k0=r*dt-rho*k*theta*dt/epsilon;
-k1=gamma1*dt*(k*rho/epsilon-0.5)-rho/epsilon;
-k2=gamma2*dt*(k*rho/epsilon-0.5)+rho/epsilon;
-k3=gamma1*dt*(1-rho^2);
-k4=gamma2*dt*(1-rho^2);
+%--- DISCRETIZE X ---------------------------------------------------------
+X(1, :) = X0;
+% Central discretization scheme
+gamma1 = 0.5;
+gamma2 = 0.5; 
+
+% Discretization (cf slide 27) :
+k0 = r * dt - rho * k * theta * dt / epsilon;
+k1 = gamma1 * dt * (k * rho / epsilon - 0.5) - rho / epsilon;
+k2 = gamma2 * dt * (k * rho / epsilon - 0.5) + rho / epsilon;
+k3 = gamma1 * dt * (1 - rho^2);
+k4 = gamma2 * dt * (1 - rho^2);
+
 for i=1:N
-    X(i+1,:)=exp(log(X(i,:))+k0+k1*V(i,:)+k2*V(i+1,:)+...
-        sqrt(k3*V(i,:)+k4*V(i+1,:)).*randn(1,Nsim));
+    % EQ 33 :
+    X(i + 1, :) = exp(log(X(i, :)) + k0 + k1 * V(i, :) + k2 * V(i + 1, :)...
+        + sqrt(k3 * V(i, :) + k4 * V(i + 1, :)) .* randn(1, Nsim));
 end
